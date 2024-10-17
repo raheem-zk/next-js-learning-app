61404cb5c2bcf88fa392a559991ca49775632f7c
raheem-zk <muhammedraheem144@gmail.com>
2024-10-17 12:48:44 +0530

    page routing and added page layout to setup

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <h1>Heading ==== 1</h1>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-green-400 w-full h-96">
+      hlo
+    </div>
+  );
+}
